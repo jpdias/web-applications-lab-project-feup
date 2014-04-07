b5a145f84e908d2a3012f685d7a0430c1aab04d3
joaoteixeirasa <joaoteixeirasa@hotmail.com>
2014-04-07 10:33:33 +0100

    Fixed db ldd and db data
    Changes:
    - Change somes files's names;
    - Fixed db ldd and db data.

--- SQL/db_data.sql
+++ SQL/db_data.sql
@@ -5,21 +5,23 @@ INSERT INTO city VALUES (3, 'Coimbra');
 INSERT INTO repaircompany VALUES (1, 'Reparações & Reparações, Ltd.', 1);
 INSERT INTO repaircompany VALUES (2, 'Clínica dos Equipamentos, Ltd.', 2);
 
+INSERT INTO inventory VALUES (1, 'inventório principal');
+
 INSERT INTO admin VALUES (1, 'admin', 'admin');
 
 INSERT INTO reader VALUES (1, 'reader1', 'passwordreader1', 'rua do reader 1', '1993-01-01', 'reader1@email.com', 'reader', '', 'active');
 INSERT INTO reader VALUES (2, 'reader2', 'passwordreader2', 'rua do reader 2', '1993-02-02', 'reader2@email.com', 'reader', '', 'active');
-INSERT INTO reader VALUES (1, 'reader3', 'passwordreader3', 'rua do reader 3', '1993-03-03', 'reader3@email.com', 'reader', '', 'active');
+INSERT INTO reader VALUES (3, 'reader3', 'passwordreader3', 'rua do reader 3', '1993-03-03', 'reader3@email.com', 'reader', '', 'active');
 
-INSERT INTO manager VALUES (1, 1);
-
-INSERT INTO requisition VALUES (1, ' ', '2014-07-01', '2014-01-06', '2014-01-01', 1, 'closed', 1, 1);
-INSERT INTO requisition VALUES (1, ' ', '2014-07-02', '2014-02-06', '2014-02-01', 1, 'closed', 2, 2);
-
-INSERT INTO reserve VALUES (1, '2014-03-04', '2014-03-01', 'closed', 1, 1);
+INSERT INTO manager VALUES (1, 1, 1);
 
 INSERT INTO item VALUES (1, 'projetor benq', 'http://www.gestorax.pt/projetorbenq.jpg', 'projetor benq modelo 2013', 'http://www.gestorax.pt/projetorbenq', 'available', 1);
 INSERT INTO item VALUES (2, 'projetor sony', 'http://www.gestorax.pt/projetorsony.jpg', 'projetor sony modelo 2014', 'http://www.gestorax.pt/projetorsony', 'available', 1);
+
+INSERT INTO requisition VALUES (1, ' ', '2014-07-01', '2014-01-06', '2014-01-01', 1, 'closed', 1, 1);
+INSERT INTO requisition VALUES (2, ' ', '2014-07-02', '2014-02-06', '2014-02-01', 1, 'closed', 2, 2);
+
+INSERT INTO reserve VALUES (1, '2014-03-04', '2014-03-01', 'closed', 1, 1);
 
 INSERT INTO tag VALUES (1, 'pojetor');
 INSERT INTO tag VALUES (2, 'imagem');
@@ -32,6 +34,4 @@ INSERT INTO itemtag VALUES (2, 1);
 INSERT INTO itemtag VALUES (2, 2);
 INSERT INTO itemtag VALUES (2, 3);
 
-INSERT INTO repair VALUES ('2013-01-04', '2013-01-01', 'sobreaquecimento', 1);
-
-INSERT INTO inventory VALUES (1, 'inventório principal');
+INSERT INTO repair VALUES (1, '2013-01-04', '2013-01-01', 'sobreaquecimento', 1, 1);
---
+++ SQL/db_ldd.sql
@@ -0,0 +1,153 @@
+DROP TABLE IF EXISTS alert CASCADE;
+DROP TABLE IF EXISTS repair CASCADE;
+DROP TABLE IF EXISTS itemtag CASCADE;
+DROP TABLE IF EXISTS tag CASCADE;
+DROP TABLE IF EXISTS waitinglist CASCADE;
+DROP TABLE IF EXISTS reserve CASCADE;
+DROP TABLE IF EXISTS requisition CASCADE;
+DROP TABLE IF EXISTS item CASCADE;
+DROP TABLE IF EXISTS manager CASCADE;
+DROP TABLE IF EXISTS reader CASCADE;
+DROP TABLE IF EXISTS admin CASCADE;
+DROP TABLE IF EXISTS inventory CASCADE;
+DROP TABLE IF EXISTS repaircompany CASCADE;
+DROP TABLE IF EXISTS city CASCADE;
+
+CREATE TABLE city
+(
+idCity integer not null primary key,
+name text not null
+);
+
+CREATE TABLE repaircompany
+(
+idRepaircompany integer not null primary key,
+companyname text not null,
+idCity integer REFERENCES city(idCity) not null
+);
+
+CREATE TABLE inventory
+(
+idInventory integer not null primary key,
+name text not null
+);
+
+CREATE TABLE admin
+(
+idAdmin integer not null primary key,
+username text unique not null,
+password text unique not null
+);
+
+DROP TYPE IF EXISTS readerstatus;
+
+CREATE TYPE readerstatus AS enum ('blocked', 'active', 'closed');
+
+CREATE TABLE reader
+(
+idReader integer not null primary key,
+username text unique not null,
+password text unique not null,
+address text,
+birthdate date,
+email text unique not null,
+firstname text not null,
+lastname text,
+currentstatus readerstatus not null
+);
+
+CREATE TABLE manager
+(
+idManager integer not null primary key,
+idInventory integer REFERENCES inventory(idInventory) not null,
+idReader integer REFERENCES reader(idReader) not null
+);
+
+DROP TYPE IF EXISTS itemstatus;
+
+CREATE TYPE itemstatus AS enum ('available', 'repair', 'withdrawn', 'unavailable');
+
+CREATE TABLE item
+(
+idItem integer not null primary key,
+name text not null,
+image text,
+description text not null,
+qrcode text,
+currentstatus itemstatus not null,
+idInventory integer REFERENCES inventory(idInventory) not null
+);
+
+DROP TYPE IF EXISTS requisitionstatus;
+
+CREATE TYPE requisitionstatus AS enum ('closed', 'open');
+
+CREATE TABLE requisition
+(
+idRequistion integer not null primary key,
+comment text not null,
+finaldate date not null,
+deliverydate date not null check(deliverydate <= finaldate),
+initialdate date not null check(initialdate <= finaldate),
+numberofrenewals integer not null check(numberofrenewals >= 0),
+currentstatus requisitionstatus not null,
+idReader integer REFERENCES reader(idReader) not null,
+idItem integer REFERENCES item(idItem) not null
+);
+
+DROP TYPE IF EXISTS reservestatus;
+
+CREATE TYPE reservestatus AS enum ('closed', 'open');
+
+CREATE TABLE reserve
+(
+idReserve integer not null primary key,
+expiredate date not null,
+reservedate date not null check(reservedate <= expiredate),
+currentstatus reservestatus not null,
+idReader integer REFERENCES reader(idReader) not null,
+idItem integer REFERENCES item(idItem) not null
+);
+
+CREATE TABLE waitinglist
+(
+idReader integer REFERENCES reader(idReader) not null,
+idItem integer REFERENCES item(idItem) not null
+);
+
+CREATE TABLE tag
+(
+idTag integer not null primary key,
+name text not null
+);
+
+CREATE TABLE itemtag
+(
+idItem integer REFERENCES item(idItem) not null,
+idTag integer REFERENCES tag(idTag) not null
+);
+
+CREATE TABLE repair
+(
+idRepair integer not null primary key,
+finaldate date not null,
+initialdate date not null check(initialdate <= finaldate),
+reason text not null,
+idRepaircompany integer REFERENCES repaircompany(idRepaircompany) not null,
+idItem integer REFERENCES item(idItem) not null
+);
+
+DROP TYPE IF EXISTS alertcategory;
+
+CREATE TYPE alertcategory AS enum ('requestreceipt', 'expiration', 'itemavailability', 'receptionreceipt','confirmationemail');
+
+CREATE TABLE alert
+(
+idAlert integer not null primary key,
+catergory alertcategory not null,
+content text not null,
+createdon date not null,
+idItem integer REFERENCES item(idItem),
+idReader integer REFERENCES reader(idReader) not null,
+idManager integer REFERENCES manager(idManager)
+);
